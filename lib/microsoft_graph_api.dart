11d20bcd86da4edc7cdfd6371f6bdbda18fe68d6
Julian Dühnen <julianduehnen@gmail.com>
2023-08-18 22:21:56 +0200

    close() method for all dio clients

--- lib/microsoft_graph_api.dart
+++ lib/microsoft_graph_api.dart
@@ -38,4 +38,20 @@ class MSGraphAPI {
     meetingRooms = MeetingRooms(_token);
     places = Places(_token);
   }
+
+  /// Shuts down all dio clients associated with this instance.
+  ///
+  /// If [force] is false (the default) the [Dio] will be kept alive until all
+  /// active connections are done. If [force] is true any active connections
+  /// will be closed to immediately release all resources. These closed
+  /// connections will receive an error event to indicate that the client was
+  /// shut down. In both cases trying to establish a new connection after
+  /// calling [close] will throw an exception.
+  void close({bool force = false}) {
+    me.close(force: force);
+    users.close(force: force);
+    calendars.close(force: force);
+    meetingRooms.close(force: force);
+    places.close(force: force);
+  }
 }
--- lib/src/meetingrooms/meetingrooms.dart
+++ lib/src/meetingrooms/meetingrooms.dart
@@ -41,4 +41,16 @@ class MeetingRooms {
       log('Failed to book meeting room: $e');
     }
   }
+
+  /// Shuts down the dio client associated with this instance.
+  ///
+  /// If [force] is false (the default) the [Dio] will be kept alive until all
+  /// active connections are done. If [force] is true any active connections
+  /// will be closed to immediately release all resources. These closed
+  /// connections will receive an error event to indicate that the client was
+  /// shut down. In both cases trying to establish a new connection after
+  /// calling [close] will throw an exception.
+  void close({bool force = false}) {
+    _dio.close(force: force);
+  }
 }
